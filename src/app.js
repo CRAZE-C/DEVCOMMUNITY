f417823f19f9e84234536c85f6c6816a1d2d1bdb
Dharan Raj <dharanraj1010@gmail.com>
2025-04-10 16:47:03 +0530

    dynamic user input

--- src/app.js
+++ src/app.js
@@ -3,14 +3,11 @@ const { connectDB } = require('./config/database');
 const User = require('./models/user');
 const app = express();
 
+app.use(express.json()); //middleware for converting json into JS object...
+
 app.post("/signup", async (req, res) => {
-    const user = new User({
-        firstName: "Dharan",
-        lastName: "Raj",
-        email: "dharanraj@gmail.com",
-        gender: "Male",
-        age: 20
-    })
+
+    const user = new User(req.body); //Dynamic way of getting input from user...
 
     try {
         await user.save();
@@ -24,7 +21,6 @@ connectDB()
     .then(() => {
         console.log("Database connection is established...");
         app.listen(1010, console.log("Server is successfully listening on port 1010"));
-
     })
     .catch((err) => {
         console.log("Database is failed to connect!!!");
