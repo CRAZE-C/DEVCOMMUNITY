149d724af7e7f8bfff56a8e756dcbc1c496fcbaa
Dharan Raj <dharanraj1010@gmail.com>
2025-04-22 11:13:32 +0530

    pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -29,7 +29,6 @@ userRouter.get('/user/request/received', userAuth, async (req, res) => {
 
 userRouter.get('/user/connections', userAuth, async (req, res) => {
     const loggedInUser = req.user._id;
-    console.log(loggedInUser)
     const connectionRequests = await ConnectionRequest.find({
         $or: [
             { toUserId: loggedInUser, status: "accepted" },
@@ -49,33 +48,34 @@ userRouter.get('/user/connections', userAuth, async (req, res) => {
 userRouter.get('/user/feed', userAuth, async (req, res) => {
     try {
         const loggedInUser = req.user;
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit > 50 ? 50 : limit;
+        const skip = (page - 1) * limit;
 
         const notToShowFeed = await ConnectionRequest.find({
             $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }]
-        }).select("fromUserId toUserId");//.populate("fromUserId","firstName").populate("toUserId","firstName");
+        }).select("fromUserId toUserId");
 
         const excludedUsers = new Set();
         notToShowFeed.forEach((req) => {
             excludedUsers.add(req.fromUserId.toString());
             excludedUsers.add(req.toUserId.toString());
         });
-        console.log(excludedUsers);
+        console.log(excludedUsers)
         const feedUsers = await User.find({
-            $and: [
-                { _id: { $nin: Array.from(excludedUsers) } },
-                // {_id: {$ne: loggedInUser}}
-            ]
-        }).select(USER_SAFE_DATA);
+            _id: { $nin: Array.from(excludedUsers) }
+        }).select(USER_SAFE_DATA).skip(skip).limit(limit);
 
-        res.status(200).send({
+        res.status(200).json({
             message: feedUsers
-        })
+        });
     }
     catch (err) {
         res.status(400).json({
             message: "ERROR : " + err.message
-        });
+        })
     }
-});
+})
 
 module.exports = userRouter;
