7886bc18130b21af8a8c7be843b627a9b91dc0da
Dharan Raj <dharanraj1010@gmail.com>
2025-05-30 11:11:11 +0530

    transfered db_uri to .env

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     await mongoose.connect(
-        "mongodb+srv://Dharan-Raj:AYv2xpw3SKmJKHwU@solitary-knight.livos.mongodb.net/devCommunity"
+        process.env.DATABASE_URI
     );
 }
 
